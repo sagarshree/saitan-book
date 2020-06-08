89cc7531d2b0ad7691c739465ff06bb93933ce08
shree <shreesagar48@gmail.com>
2020-06-08 09:17:11 +0545

    Updates

---
+++ precache-manifest.7d120bc8543b0e91a85883189044ba45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc427ca2f7054cfd436367004656493d",
+    "url": "/saitan-book/index.html"
+  },
+  {
+    "revision": "11e1187752418a230b53",
+    "url": "/saitan-book/static/css/main.9b8c2bd3.chunk.css"
+  },
+  {
+    "revision": "ef6aa4c9c0b65164e432",
+    "url": "/saitan-book/static/js/2.e84d26c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saitan-book/static/js/2.e84d26c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e1187752418a230b53",
+    "url": "/saitan-book/static/js/main.03a2baca.chunk.js"
+  },
+  {
+    "revision": "0d293dee29d182f1b3e0",
+    "url": "/saitan-book/static/js/runtime-main.782fc9b5.js"
+  }
+]);
